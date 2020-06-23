ed723f1138ac5e6682fd514b1233cc33dc9baa0d
Jacherr <jwc2002@outlook.com>
2020-06-24 00:57:04 +0100

    export new stuff

--- index.ts
+++ index.ts
@@ -2,5 +2,8 @@ import * as Client from './src/client';
 import * as Errors from './src/errors';
 import * as Routes from './src/routes';
 import * as Types from './src/types';
+import * as Collection from './src/collection';
+import * as Constants from './src/constants';
+import * as Bucket from './src/bucket'
 
-export { Client, Errors, Routes, Types };
+export { Client, Errors, Routes, Types, Collection, Constants, Bucket };
