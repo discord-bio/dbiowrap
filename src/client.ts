9d945f4c39a06025b7f0dbaa21e1034ab877e321
Jacherr <jwc2002@outlook.com>
2020-07-10 23:52:53 +0100

    fox type mismatch

--- src/client.ts
+++ src/client.ts
@@ -17,118 +17,121 @@ export interface ClientOptions {
  * The main REST client for interfacing with discord.bio.
  */
 export class Client {
-    /**
-     * The ratelimit bucket that helps prevent ratelimits. Not implemented yet
-     */
-    public bucket: Bucket = new Bucket();
+  /**
+   * The ratelimit bucket that helps prevent ratelimits. Not implemented yet
+   */
+  public bucket: Bucket = new Bucket();
 
-    /**
-     * The most recently recieved ratelimit headers, if any.
-     */
-    public ratelimitHeaders: RatelimitHeaders = {
-      [HeaderNames.RATELIMIT_RESET]: null,
-      [HeaderNames.RATELIMIT_REMAINING]: null,
-      [HeaderNames.RATELIMIT_LIMIT]: null
-    }
+  /**
+   * The most recently recieved ratelimit headers, if any.
+   */
+  public ratelimitHeaders: RatelimitHeaders = {
+    [HeaderNames.RATELIMIT_RESET]: null,
+    [HeaderNames.RATELIMIT_REMAINING]: null,
+    [HeaderNames.RATELIMIT_LIMIT]: null
+  }
 
-    /**
-     * The cache of recently fetched user profiles.
-     */
-    public userProfiles: Collection<string, Details.Response> | null
+  /**
+   * The cache of recently fetched user profiles.
+   */
+  public userProfiles: Collection<string, Details.Response> | null
 
-    constructor (options: ClientOptions = {}) {
-      if (options.cache === false) {
-        this.userProfiles = null;
-      } else {
-        let userProfileOptions = {};
-        if (options.cache !== true && typeof options.cache?.userProfiles === 'object') {
-          userProfileOptions = options.cache?.userProfiles;
-        }
-        this.userProfiles = new Collection<string, Details.Response>(userProfileOptions);
+  constructor(options: ClientOptions = {}) {
+    if (options.cache === false) {
+      this.userProfiles = null;
+    } else {
+      let userProfileOptions = {};
+      if (options.cache !== true && typeof options.cache?.userProfiles === 'object') {
+        userProfileOptions = options.cache?.userProfiles;
       }
+      this.userProfiles = new Collection<string, Details.Response>(userProfileOptions);
     }
+  }
 
-    /**
-     * Constructs a fully-qualified path from the base URL, version, and endpoint/parameters. This function is internal and should not be used in end-user code.
-     */
-    private constructPath (endpoint: string, params?: {[key: string]: string}): string {
-      let path = `${BASE_URL}${VERSION}${endpoint}`;
-      if (params) {
-        Object.keys(params).forEach(param => {
-          path = path.replace(`${PARAM_INDICATOR}${param}`, params[param]);
-        });
-      }
-      return path;
-    }
-
-    /**
-     * Fetches a user from discord.bio.
-     * @param searchQuery The user ID or slug to search for
-     */
-    public async fetchUserDetails (searchQuery: string): Promise<Details.Response> {
-      if (this.userProfiles?.get(searchQuery)) {
-        return <Details.Response> this.userProfiles.get(searchQuery);
-      }
-      const path = this.constructPath(Endpoints.DETAILS, {
-        [Endpoints.DETAILS.split(PARAM_INDICATOR)[1]]: searchQuery
+  /**
+   * Constructs a fully-qualified path from the base URL, version, and endpoint/parameters. This function is internal and should not be used in end-user code.
+   */
+  private constructPath(endpoint: string, params?: { [key: string]: string }): string {
+    let path = `${BASE_URL}${VERSION}${endpoint}`;
+    if (params) {
+      Object.keys(params).forEach(param => {
+        path = path.replace(`${PARAM_INDICATOR}${param}`, params[param]);
       });
-      const res: Details.Response = await this.request(path).then((res: Details.Response) => ({
-        payload: {
-          user: {
-            ...res.payload.user,
-            details: new Details.Details(res.payload.user.details)
-          },
-          discord: new Details.Discord(res.payload.discord)
-        }
-      }));
-
-      if (this.userProfiles) this.userProfiles.set(searchQuery, res);
-      return res;
     }
+    return path;
+  }
 
-    /**
-     * Fetches the top liked users on discord.bio.
-     */
-    public async fetchTopUsers (): Promise<TopLikes.Response> {
-      const path = this.constructPath(Endpoints.TOP_LIKES);
-      return this.request(path).then((res: TopLikes.Response) => ({
-        payload: res.payload.map(x => ({
+  /**
+   * Fetches a user from discord.bio.
+   * @param searchQuery The user ID or slug to search for
+   */
+  public async fetchUserDetails(searchQuery: string): Promise<Details.Response> {
+    if (this.userProfiles?.get(searchQuery)) {
+      return <Details.Response>this.userProfiles.get(searchQuery);
+    }
+    const path = this.constructPath(Endpoints.DETAILS, {
+      [Endpoints.DETAILS.split(PARAM_INDICATOR)[1]]: searchQuery
+    });
+    const res: Details.Response = await this.request(path).then((res: Details.Response) => ({
+      payload: {
+        user: {
+          ...res.payload.user,
+          details: new Details.Details(res.payload.user.details)
+        },
+        discord: new Details.Discord(res.payload.discord)
+      }
+    }));
+
+    if (this.userProfiles) this.userProfiles.set(searchQuery, res);
+    return res;
+  }
+
+  /**
+   * Fetches the top liked users on discord.bio.
+   */
+  public async fetchTopUsers(): Promise<TopLikes.Response> {
+    const path = this.constructPath(Endpoints.TOP_LIKES);
+    return this.request(path).then((res: TopLikes.Response) => ({
+      payload: {
+        users: res.payload.users.map(x => ({
           ...x,
           discord: new TopLikes.DiscordUser(x.discord)
-        }))
-      }));
-    }
-
-    /**
-     * Sends a request to a specified path and return the response as json. Mostly for internal use. This function updates the stored ratelimit headers.
-     * @param path The path to send the request to
-     */
-    public async request (path: string): Promise<any> {
-      const request = new Request(path);
-      const response = await fetch(request);
-      this.updateRatelimitHeaders(response.headers);
-      let json: any = await response.text();
-      try {
-        json = JSON.parse(json)
-      } catch (e) {}
-      if (![StatusCodes.SUCCESS, StatusCodes.RATELIMIT].includes(response.status)) {
-        const throwValue = json.message || json;
-        throw new DiscordBioError(throwValue, request, response);
-      } else if (response.status === StatusCodes.RATELIMIT) {
-        const throwValue = json.message || json;
-        throw new RatelimitError(throwValue, request, response);
+        })),
+        totalPages: res.payload.totalPages
       }
-      return json;
-    }
+    }));
+  }
 
-    /**
-     * Updates the stored ratelimit headers. This function is internal and should not be used in end-user code.
-     */
-    private updateRatelimitHeaders (headers: Headers) {
-      const ratelimitHeaders = [HeaderNames.RATELIMIT_RESET, HeaderNames.RATELIMIT_REMAINING, HeaderNames.RATELIMIT_RESET];
-      for (const headerName of ratelimitHeaders) {
-        const headerValue = headers.get(headerName);
-        if (headerValue) this.ratelimitHeaders[headerName] = parseInt(headerValue);
-      }
+  /**
+   * Sends a request to a specified path and return the response as json. Mostly for internal use. This function updates the stored ratelimit headers.
+   * @param path The path to send the request to
+   */
+  public async request(path: string): Promise<any> {
+    const request = new Request(path);
+    const response = await fetch(request);
+    this.updateRatelimitHeaders(response.headers);
+    let json: any = await response.text();
+    try {
+      json = JSON.parse(json)
+    } catch (e) { }
+    if (![StatusCodes.SUCCESS, StatusCodes.RATELIMIT].includes(response.status)) {
+      const throwValue = json.message || json;
+      throw new DiscordBioError(throwValue, request, response);
+    } else if (response.status === StatusCodes.RATELIMIT) {
+      const throwValue = json.message || json;
+      throw new RatelimitError(throwValue, request, response);
     }
+    return json;
+  }
+
+  /**
+   * Updates the stored ratelimit headers. This function is internal and should not be used in end-user code.
+   */
+  private updateRatelimitHeaders(headers: Headers) {
+    const ratelimitHeaders = [HeaderNames.RATELIMIT_RESET, HeaderNames.RATELIMIT_REMAINING, HeaderNames.RATELIMIT_RESET];
+    for (const headerName of ratelimitHeaders) {
+      const headerValue = headers.get(headerName);
+      if (headerValue) this.ratelimitHeaders[headerName] = parseInt(headerValue);
+    }
+  }
 }
