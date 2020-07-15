bdf6200902e592bc6eaa0fa6f9c47e6e76652891
Jacher <jwc2002@outlook.com>
2020-07-15 11:35:17 +0100

    move index.ts to src

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import * as RestClient from './rest/restclient';
+import * as Errors from './rest/errors';
+import * as Routes from './rest/routes';
+import * as Types from './rest/types';
+import * as Collection from './collection';
+import * as RestConstants from './rest/constants';
+import * as Bucket from './rest/bucket';
+import * as User from './structures/discorduser';
+import * as Socket from './gateway/socket';
+import * as SocketManager from './gateway/socketmanager';
+import * as GatewayConstants from './gateway/constants';
+import * as GatewayEvents from './gateway/gatewayevents';
+import * as Client from './client';
+
+export {
+  RestClient,
+  Errors,
+  Routes,
+  Types,
+  Collection,
+  RestConstants,
+  Bucket,
+  User,
+  Socket,
+  SocketManager,
+  GatewayConstants,
+  GatewayEvents,
+  Client
+};
